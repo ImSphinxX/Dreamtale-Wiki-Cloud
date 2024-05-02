aafd419509dbc5b76b3c22465390f50deafdb17a
ImSphinxX <139464679+ImSphinxX@users.noreply.github.com>
2024-05-02 13:45:00 +0100

    Paragrafo Condiviso Colore

--- Monster.js
+++ Monster.js
@@ -2,16 +2,22 @@ function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     const windowHeight = window.innerHeight;
     const sectionHeight = section.clientHeight;
-    const offset = (windowHeight - sectionHeight) / 100; // Calcolo dell'offset per centrare verticalmente
+    const offset = (windowHeight - sectionHeight) / 100;
 
-    section.scrollIntoView({ behavior: "smooth", block: "center" }); // Scorrimento centrato
-    window.scrollBy(0, -offset); // Correzione per centrare verticalmente
+    //scorrimento:
+    section.scrollIntoView({ behavior: "smooth", block: "center" });
+    window.scrollBy(0, -offset);
     document.getElementById(sectionId).style.backgroundColor = "rgb(57, 57, 57)";
-    
+
+    //colorazione sfondo.
     var allElements = document.querySelectorAll("[id]");
     allElements.forEach(function(element) {
         if (!element.classList.contains("card") && element.id !== sectionId) {
             element.style.backgroundColor = "rgb(17, 17, 17)";
         }
     });
+
+    //colorazione per il paragrafo condiviso
+    var firstParagraphBackgroundColor = window.getComputedStyle(document.getElementById("Undead-txt")).backgroundColor;
+    document.getElementById("Shared-txt").style.backgroundColor = firstParagraphBackgroundColor;
 }
